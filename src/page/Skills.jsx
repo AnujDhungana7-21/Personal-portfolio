62c9351463436eb162b5b4c385fc31050a635504
Anuj Dhungana <hoomandaws1999@gmail.com>
2024-05-17 23:02:34 +0545

    some css change

--- src/page/Skills.jsx
+++ src/page/Skills.jsx
@@ -31,15 +31,15 @@ export const Skills = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 gap-4  ">
           {data.map((el) => {
             return (
-              <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+              <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow  ">
                 <div class="p-5">
                   <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight  dark:text-white text-center text-primary-orange ">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight   text-center text-primary-orange ">
                       {el.Title}
                     </h5>
                   </a>
                   <div>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    <p class="mb-3 font-normal text-primary-blue">
                       {el.subTitle.name.map((el) => {
                         return (
                           <>
